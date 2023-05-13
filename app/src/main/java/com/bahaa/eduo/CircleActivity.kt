cdcf0aeb4a8dfbbf678501ec860a9904d08b3da8
Bahaa Fathi <BahaaFathi@outlook.sa>
2023-05-13 12:42:56 +0300

    finalize project

--- app/src/main/java/com/bahaa/eduo/CircleActivity.kt
+++ app/src/main/java/com/bahaa/eduo/CircleActivity.kt
@@ -1,11 +1,46 @@
 package com.bahaa.eduo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class CircleActivity : AppCompatActivity() {
+
+    private lateinit var areaEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_circle)
+
+        areaEditText = findViewById(R.id.area_edit_text)
+        val previousButton = findViewById<View>(R.id.previous_button)
+        val nextButton = findViewById<View>(R.id.next_button)
+
+        previousButton.setOnClickListener {
+            finish()
+        }
+
+        nextButton.setOnClickListener {
+            val areaString = areaEditText.text.toString()
+            val area = areaString.toIntOrNull()
+
+            if ((area != null) && (area > 0) && area.toInt() == calculateCircleArea().toInt()) {
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra("result", "true")
+                startActivity(intent)
+            } else {
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra("result", "false")
+                startActivity(intent)
+            }
+        }
     }
+
+    private fun calculateCircleArea(): Double {
+        val pi = 3.14159
+        return pi * 4 * 4
+    }
+
 }
--- app/src/main/java/com/bahaa/eduo/RectangleActivity.kt
+++ app/src/main/java/com/bahaa/eduo/RectangleActivity.kt
@@ -1,11 +1,44 @@
 package com.bahaa.eduo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class RectangleActivity : AppCompatActivity() {
+
+    private lateinit var areaEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rectangle)
+
+        areaEditText = findViewById(R.id.area_edit_text)
+        val previousButton = findViewById<View>(R.id.previous_button)
+        val nextButton = findViewById<View>(R.id.next_button)
+
+        previousButton.setOnClickListener {
+            finish()
+        }
+
+        nextButton.setOnClickListener {
+            val areaString = areaEditText.text.toString()
+            val area = areaString.toIntOrNull()
+
+            if ((area != null) && (area > 0) && area == calculateRectangleArea()) {
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra("result", "true")
+                startActivity(intent)
+            } else {
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra("result", "false")
+                startActivity(intent)
+            }
+        }
+    }
+
+    private fun calculateRectangleArea(): Int {
+        return 10 * 6
     }
 }
